6c3f35c0ebfa302babf7dc647b24282a85f736a8
dinesh <dusnaedineshpradip@gmail.com>
2020-09-27 12:44:33 +0000

    demo file modified

--- demo.sh
+++ demo.sh
@@ -4,4 +4,4 @@ port=80
 tag=$(cat /tmp/version.txt)
 
 docker rm -f $env
-docker run -itd --name $env -p $port:$port dinesh7020/devops-demo:$tag 
+docker run -itd --name $env -p $port:$port dinesh7020/docker_repo:$tag 
